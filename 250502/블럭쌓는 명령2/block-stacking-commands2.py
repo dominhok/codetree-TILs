7ab852e980d8669bf2081e64b0ad5b83897dd109
dominhok <121854387+dominhok@users.noreply.github.com>
2025-05-02 21:16:58 +0900

    Title: 블럭쌓는 명령2, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250502/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,12 +2,11 @@ n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-answer = [0] * n
+answer = [0] * (n+1)
 
 for i in range(k):
     s, e = commands[i]
     for i in range(s,e+1):
         answer[i] +=1
 
-answer.sort(reverse = True)
-print(answer[0])
+print(max(answer))
