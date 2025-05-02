6cd2a1a282a4b650cc86550bf65930f573f5234a
dominhok <121854387+dominhok@users.noreply.github.com>
2025-05-02 21:11:12 +0900

    Title: 블럭쌓는 명령2, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250502/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+commands = [tuple(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
+answer = [0] * n
+
+for i in range(k):
+    s, e = commands[i]
+    for i in range(s,e+1):
+        answer[i] +=1
+
+answer.sort(reverse = True)
+print(answer[0])
