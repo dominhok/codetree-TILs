f2669290f60858212774dc9b9893161faee94cd7
dominhok <121854387+dominhok@users.noreply.github.com>
2025-05-02 19:20:33 +0900

    Title: Time to Time, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250502/Time to Time/time-to-time.py
@@ -0,0 +1,19 @@
+a, b, c, d = map(int, input().split())
+
+# Please write your code here.
+
+hours,mins = a,b
+elapsed_time = 0
+
+while True:
+    if hours == c and mins == d:
+        break
+
+    elapsed_time +=1
+    mins +=1
+
+    if mins == 60:
+        hours +=1
+        mins = 0
+
+print(elapsed_time)
